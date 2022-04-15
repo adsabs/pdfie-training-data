21897ed18992ffa754810895e81f5670ef9d95aa
Peter Williams <peter@newton.cx>
2022-04-15 11:59:02 -0400

    collections/esasp/index.py: fix up formatting

--- collections/esasp/index.py
+++ collections/esasp/index.py
@@ -13,12 +13,12 @@ import hashlib
 import os.path
 import toml
 
-REFERENCES_PREFIX = os.environ.get('ADS_REFERENCES', '/proj/ads/references')
-ARTICLES_PREFIX = os.environ.get('ADS_ARTICLES', '/proj/ads/articles')
+REFERENCES_PREFIX = os.environ.get("ADS_REFERENCES", "/proj/ads/references")
+ARTICLES_PREFIX = os.environ.get("ADS_ARTICLES", "/proj/ads/articles")
 COLL_PREFIX = os.path.dirname(__file__)
 
-REFERENCES_SUBDIR = 'sources/ESASP'
-ARTICLES_PDF_SUBDIR_TMPL = 'bitmaps/seri/ESASP/{vol}/PDF/{bibcode}.pdf'
+REFERENCES_SUBDIR = "sources/ESASP"
+ARTICLES_PDF_SUBDIR_TMPL = "bitmaps/seri/ESASP/{vol}/PDF/{bibcode}.pdf"
 
 
 def do_one_doc(refdirpath: str, reffn: str):
@@ -28,25 +28,25 @@ def do_one_doc(refdirpath: str, reffn: str):
     """
 
     bibcode, ext = os.path.splitext(reffn)
-    if ext != '.raw':
+    if ext != ".raw":
         return
 
     vol = os.path.basename(refdirpath)
 
     # Hack!
-    bibcode = bibcode.replace('ESASP.624', 'soho...18')
+    bibcode = bibcode.replace("ESASP.624", "soho...18")
 
     pdf_sub_path = ARTICLES_PDF_SUBDIR_TMPL.format(vol=vol, bibcode=bibcode)
     pdf_path = os.path.join(ARTICLES_PREFIX, pdf_sub_path)
 
     # Analyze PDF file
 
-    b = bytearray(128*1024)
+    b = bytearray(128 * 1024)
     mv = memoryview(b)
     h = hashlib.sha256()
     n_bytes = 0
 
-    with open(pdf_path, 'rb', buffering=0) as f:
+    with open(pdf_path, "rb", buffering=0) as f:
         while n := f.readinto(mv):
             h.update(mv[:n])
             n_bytes += n
@@ -54,18 +54,17 @@ def do_one_doc(refdirpath: str, reffn: str):
     # Emit
 
     metadata = {
-        'bibcode': bibcode,
-        'pdf_sha256': h.hexdigest(),
-        'pdf_n_bytes': n_bytes,
-        'pdf_path': '$ADS_ARTICLES/' + pdf_sub_path,
+        "bibcode": bibcode,
+        "pdf_sha256": h.hexdigest(),
+        "pdf_n_bytes": n_bytes,
+        "pdf_path": "$ADS_ARTICLES/" + pdf_sub_path,
     }
 
-    toml_path = os.path.join(COLL_PREFIX, vol, bibcode + '.doc.toml')
+    toml_path = os.path.join(COLL_PREFIX, vol, bibcode + ".doc.toml")
     print(toml_path)
 
-    with open(toml_path, 'wt') as f:
+    with open(toml_path, "wt") as f:
         toml.dump(metadata, f)
-
 
 
 def main():
@@ -76,5 +75,5 @@ def main():
             do_one_doc(dirpath, fn)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
