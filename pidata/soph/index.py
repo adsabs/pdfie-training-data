9a2f014f317aea4ce79cdd98f81e6a2bc5e36287
Peter Williams <peter@newton.cx>
2022-04-29 11:23:41 -0400

    soph: work on indexing

--- pidata/soph/index.py
+++ pidata/soph/index.py
@@ -46,104 +46,211 @@ There are also JATS files with no "unstructured" version of the reference, e.g.
 
 from dataclasses import dataclass
 import hashlib
+import html.entities
 import os.path
+from pathlib import Path
+import random
+import sys
 import toml
-from typing import Dict
+from typing import Dict, TextIO
 
-REFERENCES_PREFIX = os.environ.get("ADS_REFERENCES", "/proj/ads/references")
-FULLTEXT_PREFIX = os.environ.get("ADS_FULLTEXT", "/proj/ads/fulltext")
-ABSTRACTS_PREFIX = os.environ.get("ADS_ABSTRACTS", "/proj/ads/abstracts")
-COLL_PREFIX = os.path.dirname(__file__)
+
+def envpath(env_var_name: str, default: str) -> Path:
+    return Path(os.environ.get(env_var_name, default))
+
+REFERENCES_PREFIX = envpath("ADS_REFERENCES", "/proj/ads/references")
+FULLTEXT_PREFIX = envpath("ADS_FULLTEXT", "/proj/ads/fulltext")
+ABSTRACTS_PREFIX = envpath("ADS_ABSTRACTS", "/proj/ads/abstracts")
+COLL_PREFIX = Path(os.path.dirname(__file__))
+FULLTEXT_LINKS_PATH = ABSTRACTS_PREFIX / "config/links/fulltext/all.links"
 
 REFERENCES_SUBDIR = "sources/SoPh/"
 FULLTEXT_SUBDIR = "sources/SoPh/"
+PDFS_ARE_RASTER = False
 
-FULLTEXT_LINKS_PATH = os.path.join(ABSTRACTS_PREFIX, "config/links/fulltext/all.links")
 
 @dataclass
 class Doc:
     bibcode: str = None
     pdf_path: str = None
-    xml_path: str = None
+
+    def create_toml_metadata(self):
+        # Rewrite the PDF path to use $ADS_ARTICLES as the prefix instead of
+        # $ADS_FULLTEXT so that I can get away with setting up fewer mounts when
+        # doing Docker or sshfs-based processing.
+
+        ads_pdf_path = self.pdf_path.replace(str(FULLTEXT_PREFIX), '$ADS_ARTICLES/fulltext')
+
+        # Analyze PDF file
+
+        b = bytearray(128 * 1024)
+        mv = memoryview(b)
+        h = hashlib.sha256()
+        n_bytes = 0
+
+        with open(self.pdf_path, "rb", buffering=0) as f:
+            while n := f.readinto(mv):
+                h.update(mv[:n])
+                n_bytes += n
+
+        # Emit
+
+        return {
+            "bibcode": self.bibcode,
+            "pdf_sha256": h.hexdigest(),
+            "pdf_n_bytes": n_bytes,
+            "ads_pdf_path": ads_pdf_path,
+            "random_index": random.randint(0, 999999),
+            "pdf_is_raster": PDFS_ARE_RASTER,
+        }
 
 
-def scan_candidates():
-    # First scan the "links" file for bibcodes and PDF paths. Filter by
-    # fulltext path to identify the publisher PDFs. NB: `/proj/ads/fulltext`
-    # is just a symlink to `/proj/ads/articles/fulltext`, but the former
-    # is the prefix used in this file.
+def scan_candidates() -> Dict[str, Dict[str, Doc]]:
+    # Scan the "links" file for bibcodes and PDF paths. Filter by fulltext path
+    # to identify the publisher PDFs. NB: `/proj/ads/fulltext` is just a symlink
+    # to `/proj/ads/articles/fulltext`, but the former is the prefix used in
+    # this file. Group by volume to narrow down the XML loading task.
 
-    cands = {}
-    prefix = os.path.join(FULLTEXT_PREFIX, FULLTEXT_SUBDIR)
-    seen_volumes = set()
+    by_vol = {}
+    n = 0
+    prefix = str(FULLTEXT_PREFIX / FULLTEXT_SUBDIR) + os.path.sep
 
-    with open(FULLTEXT_LINKS_PATH, "rt") as f:
+    with FULLTEXT_LINKS_PATH.open("rt") as f:
         for line in f:
             pieces = line.split()
             if len(pieces) > 1 and pieces[1].startswith(prefix):
                 bibcode = pieces[0]
                 pdf_path = pieces[1]
                 doc = Doc(bibcode=bibcode, pdf_path=pdf_path)
-                cands[bibcode] = doc
 
                 volume = pdf_path[len(prefix):].split('/', 1)[0]
-                seen_volumes.add(volume)
+                by_vol.setdefault(volume, {})[bibcode] = doc
+                n += 1
 
-    print("Found {len(cands)} bibcode/publisher-PDF links")
+    print(f"Found {n} bibcode/publisher-PDF links in {len(by_vol)} volumes")
+    return by_vol
 
 
-def do_one_doc(refdirpath: str, reffn: str):
+def process_volume(vol: str, docs: Dict[str, Doc]):
+    # Currently we only have publisher PDF linkage data for volumes where there
+    # are `.springer.xml` and `.jats.xml` files, and we can only obtain the
+    # unstructured citation from `.springer.xml`.
+
+    vol_dir = REFERENCES_PREFIX / REFERENCES_SUBDIR / vol
+    print(f"Volume {vol}:")
+
+    for fn in vol_dir.iterdir():
+        if fn.name.endswith(".springer.xml"):
+            print(f"  XML {fn}:")
+            process_springer_xml(vol, docs, fn)
+
+
+class SpringerProcessor:
     """
-    refdirpath is like `/proj/ads/references/sources/ESASP/0624`
-    reffn is like `2006ESASP.624E..85B.raw`
+    These files are called XML but can't be processed with an actual XML
+    handler, because there are things like unescaped ">" characters in DOIs, no
+    single toplevel element, etc. Fortunately the format is fairly easy to
+    process manually. (It would sure be nice if people actually applied escaping
+    when generating "XML" from text.)
     """
 
-    bibcode, ext = os.path.splitext(reffn)
-    if ext != ".raw":
-        return
+    vol_dir: Path = None
+    docs: Dict[str, Doc] = None
+    cur_output: TextIO = None
+    n_skipped_no_pdf: int = 0
+    n_skipped_preexist: int = 0
+    n_created: int = 0
 
-    vol = os.path.basename(refdirpath)
+    def __init__(self, volume: str, docs: Dict[str, Doc]):
+        self.docs = docs
+        self.vol_dir = COLL_PREFIX / volume
+        self.vol_dir.mkdir(parents=True, exist_ok=True)
 
-    # Hack!
-    bibcode = bibcode.replace("ESASP.624", "soho...18")
+    def cur_done(self):
+        if self.cur_output is not None:
+            self.cur_output.close()
+            self.cur_output = None
 
-    pdf_sub_path = ARTICLES_PDF_SUBDIR_TMPL.format(vol=vol, bibcode=bibcode)
-    pdf_path = os.path.join(ARTICLES_PREFIX, pdf_sub_path)
+    def maybe_start_new(self, bibcode: str):
+        self.cur_done()
 
-    # Analyze PDF file
+        doc = self.docs.get(bibcode)
+        if doc is None:
+            self.n_skipped_no_pdf += 1
+            return
 
-    b = bytearray(128 * 1024)
-    mv = memoryview(b)
-    h = hashlib.sha256()
-    n_bytes = 0
+        toml_path = self.vol_dir / (bibcode + ".doc.toml")
+        if toml_path.exists():
+            self.n_skipped_preexist += 1
+            return
 
-    with open(pdf_path, "rb", buffering=0) as f:
-        while n := f.readinto(mv):
-            h.update(mv[:n])
-            n_bytes += n
+        with open(toml_path, "wt") as f:
+            self.n_created += 1
+            toml.dump(doc.create_toml_metadata(), f)
 
-    # Emit
+        rs_path = self.vol_dir / (bibcode + ".rs.txt")
+        self.cur_output = rs_path.open("wt")
 
-    metadata = {
-        "bibcode": bibcode,
-        "pdf_sha256": h.hexdigest(),
-        "pdf_n_bytes": n_bytes,
-        "pdf_path": "$ADS_ARTICLES/" + pdf_sub_path,
-    }
+    def handle_line(self, line):
+        line = line.strip()
+        if not line:
+            return
 
-    toml_path = os.path.join(COLL_PREFIX, vol, bibcode + ".doc.toml")
-    print(toml_path)
+        if line.startswith("<ADSBIBCODE>"):
+            bibcode = line[12:]
+            bibcode = bibcode[:bibcode.index("<")]
+            self.maybe_start_new(bibcode)
+        elif self.cur_output is not None:
+            try:
+                i = line.index("<BibUnstructured>")
+            except ValueError:
+                pass
+            else:
+                text = line[i+17:]
+                # There are cases with additional "XML" content in tags within
+                # the `<BibUnstructured>` (e.g. `<ExternalRef>`). Hopefully the
+                # XML is un-busted enough that any less-thans in the
+                # unstructured references are properly escaped.
+                text = text[:text.index("<")]
+                text = handle_entities(text)
+                print(text.strip(), file=self.cur_output)
 
-    with open(toml_path, "wt") as f:
-        toml.dump(metadata, f)
+    def all_done(self):
+        self.cur_done()
+        print(f"    Emitted data for {self.n_created} docs")
+        print(f"    Skipped {self.n_skipped_no_pdf} docs with no publisher PDF")
+        print(f"    Skipped {self.n_skipped_preexist} preexisting docs")
+
+
+def process_springer_xml(volume: str, docs: Dict[str, Doc], xml_path: Path):
+    proc = SpringerProcessor(volume, docs)
+
+    with xml_path.open("rt") as f:
+        for line in f:
+            proc.handle_line(line)
+
+    proc.all_done()
+
+
+_MORE_ENTITIES = [
+    ("&ecedil;", "ę"),
+    ("&gcaron;", "ǧ"),
+]
+
+def handle_entities(text: str) -> str:
+    text = html.unescape(text)
+
+    for ent, repl in _MORE_ENTITIES:
+        text = text.replace(ent, repl)
+
+    return text
 
 
 def main():
-    ref_root = os.path.join(REFERENCES_PREFIX, REFERENCES_SUBDIR)
+    by_vol = scan_candidates()
 
-    for dirpath, _dirnames, filenames in os.walk(ref_root):
-        for fn in filenames:
-            do_one_doc(dirpath, fn)
+    for vol, docs in by_vol.items():
+        process_volume(vol, docs)
 
 
 if __name__ == "__main__":
